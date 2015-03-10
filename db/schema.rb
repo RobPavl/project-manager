2de714bd05913041d4086816734753e5c8e1d85d
Robert Pavlovskyi <robert.pavlovsky@gmail.com>
2015-03-10 15:20:01 +0200

    Added deadline to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309192057) do
+ActiveRecord::Schema.define(version: 20150310130524) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "task_id",    limit: 4
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150309192057) do
     t.integer  "priority",   limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.datetime "deadline"
   end
 
   create_table "users", force: :cascade do |t|
