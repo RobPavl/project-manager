aca2f393e73f9732fefba96c4021947ea70d15f8
Robert Pavlovskyi <robert.pavlovsky@gmail.com>
2015-03-31 15:19:24 +0300

    Angular renamed like in database

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,18 +1,18 @@
 var app = angular.module('todoList', [])
   app.controller('TaskCtrl', ['$scope', function ($scope) {
     $scope.tasks =[
-          {"text":"Знайти квитки до Києва", "date": "26 бер. 2015", "done": false, "id": 1, comments: [{'text':'I think, it must be faster than else', 'file':false},
-  	 {'text':'Kill them all!', 'file':false}]},
-          {"text":"Cпакувати речі", "date":"27 бер. 2015", "done": true, "id": 2, comments: [{'text':'I think, it must be faster than else', 'file':false},
-  	 {'text':'Хто не скаче, той - москаль!', 'file':false}]},
-          {"text":"Поїхати на двірець", "date":"27 бер. 2015 22.15", "done": false, "id": 3, comments: [{'text':'I think, it must be faster than else', 'file':false},
-  	 {'text':'комуняку на гілляку!', 'file':false}]}]
+          {"body":"Знайти квитки до Києва", "deadline": "26 бер. 2015", "is_checked": false, "id": 1, comments: [{'body':'I think, it must be faster than else', 'file':false},
+  	 {'body':'Kill them all!', 'file':false}]},
+          {"body":"Cпакувати речі", "deadline":"27 бер. 2015", "is_checked": true, "id": 2, comments: [{'body':'I think, it must be faster than else', 'file':false},
+  	 {'body':'Хто не скаче, той - москаль!', 'file':false}]},
+          {"body":"Поїхати на двірець", "deadline":"27 бер. 2015 22.15", "is_checked": false, "id": 3, comments: [{'body':'I think, it must be faster than else', 'file':false},
+  	 {'body':'комуняку на гілляку!', 'file':false}]}]
 
     $scope.editTask = false;
 
     $scope.showEdition = function(id, $index){
       $scope.editTask = true;
-      $scope.editedText = $scope.tasks[$index].text;
+      $scope.editedText = $scope.tasks[$index].body;
       $scope.editedId = $scope.tasks[$index].id;
 	}
 
@@ -27,13 +27,13 @@ var app = angular.module('todoList', [])
     $scope.showComment = function(id, $index){
      console.log('showComment');
       $scope.comentary = true;
-      $scope.seenComment = $scope.tasks[$index].comments[0].text;
+      $scope.seenComment = $scope.tasks[$index].comments[0].body;
       $scope.taskId = $scope.tasks[$index].id;
 	}
   }]);
 
   app.controller('ProjectCtrl', ['$scope', function ($scope) {
-    $scope.projects=[{'text': "Фігня якась"},
-    {'text':'Ще одна фігня'}]
+    $scope.projects=[{'name': "Фігня якась"},
+    {'name':'Ще одна фігня'}]
   }]);
 
