74993345b5aecdcf30f9c09a4ea352dbf72e2cc8
Robert Pavlovskyi <robert.pavlovsky@gmail.com>
2015-04-06 21:04:25 +0300

    Made links in projects ng-controller

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,1 +1,38 @@
 var app = angular.module('todoList', [])
+
+  app.controller('ProjectsCtrl', ['$scope', function ($scope) {
+    $scope.projects=[{'name': "Проект 1"},{'name': "Проект 2"},
+    {'name': "Проект 3"},{'name': "Проект 4"},{'name': "Проект 5"}]
+  }]);
+
+/*  app.controller('ProjectCtrl', ['$scope', function ($scope) {
+    //$scope.
+  }]);
+
+  app.controller('TasksCtrl', ['$scope', function ($scope) {
+    //$scope.
+  }]);
+
+  app.controller('TaskCtrl', ['$scope', function ($scope) {
+    //$scope.
+  }]);
+
+  app.controller('CreateTaskCtrl', ['$scope', function ($scope) {
+    //$scope.
+  }]);
+
+  app.controller('EditTaskCtrl', ['$scope', function ($scope) {
+    //$scope.
+  }]);
+
+  app.controller('CommentsCtrl', ['$scope', function ($scope) {
+    //$scope.
+  }]);
+
+  app.controller('CommentCtrl', ['$scope', function ($scope) {
+    //$scope.
+  }]);
+
+  app.controller('CreateCommentCtrl', ['$scope', function ($scope) {
+    //$scope.
+  }]);*/
