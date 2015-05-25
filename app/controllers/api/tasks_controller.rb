da875e35d87fa82abae33545f792ee93869c1e8a
Robert Pavlovskyi <robert.pavlovsky@gmail.com>
2015-05-25 23:57:35 +0300

    hooked up ActiveModel_Serializer

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,7 +1,7 @@
 class Api::TasksController < ApplicationController
   def index
-    render json: current_user.projects.where(id: params[:project_id]).first.tasks.to_json
-    #render json: @post, serializer: TaskSerializer
+    @tasks = current_user.projects.where(id: params[:project_id]).first.tasks
+    render json: @tasks, each_serializer: TaskSerializer
   end
 
   def create
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,3 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :task_id, :body, :file
-  belongs_to :task
 end
